310a9487a0ab87c94339255226bf7aeab8e35cda
Annastark <89winteriscoming@gmail.com>
2021-11-09 16:31:19 +0100

    else?

--- js/script.js
+++ js/script.js
@@ -15,10 +15,10 @@ for (let n = 1; n <= 100; n ++)  {
     if (n % 3 == 0) {
         console.log (n + 'Fizz');
     } else if (n % 5 == 0) {
-        console.log (n + 'Buzz');} }
-    /* else (n % 3 == 0 && n % 5 == 0) {
+        console.log (n + 'Buzz');} 
+    else (n % 3 == 0 && n % 5 == 0) {
         console.log (n + 'FizzBuzz');
     }
-};*/
+};
 
 
